5d96a5e07836ca950ac9ab60d09d745e31e3ca1a
Atom1688 <atomthepro@outlook.com>
2024-05-09 11:47:22 -0300

    implementado telas de edicao

--- app.py
+++ app.py
@@ -69,6 +69,21 @@ def del_sensor():
     sensors.pop(sensor)
     return render_template("sensors/sensors.html", sensors=sensors)
 
+@app.route('/edit_sensor')
+def edit_sensor():
+    return render_template("sensors/edit_sensor.html", sensors=sensors)
+
+@app.route('/update_sensor', methods=['POST'])
+def update_sensor():
+    global sensors
+    sensor_name = request.form['sensor']
+    new_sensor_name = request.form['new_sensor_name']
+
+    if sensor_name in sensors:
+        sensors.pop(sensor_name)
+        sensors[new_sensor_name] = None
+    return render_template("sensors/sensors.html", sensors=sensors)
+
 # --------------- CRUD ACTUATOR --------------- # 
 
 @app.route('/register_actuator')
@@ -104,6 +119,21 @@ def del_actuator():
     actuators.pop(actuator)
     return render_template("actuators/actuators.html", actuators=actuators)
 
+@app.route('/edit_actuator')
+def edit_actuator():
+    return render_template("actuators/edit_actuator.html", actuators=actuators)
+
+@app.route('/update_actuator', methods=['POST'])
+def update_actuator():
+    global actuators
+    actuator_name = request.form['actuator']
+    new_actuator_name = request.form['new_actuator_name']
+
+    if actuator_name in actuators:
+        actuators.pop(actuator_name)
+        actuators[new_actuator_name] = None
+    return render_template("actuators/actuators.html", actuators=actuators)
+
 # --------------- CRUD USER --------------- # 
 
 @app.route('/register_user')
@@ -117,7 +147,7 @@ def add_user():
         user = request.form['user']
     else:
         user = request.args.get('user', None)
-    users[user] = None
+    users[user] = '0000'
     return render_template("users/users.html", users=users)
 
 @app.route('/list_users')
@@ -141,6 +171,30 @@ def del_user():
     else:
         user = request.args.get('user', None)
     users.pop(user)
+    return render_template("users/users.html", users=users)
+
+@app.route('/edit_user')
+def edit_user():
+    removable_users = {key: value for key, value in users.items() if key != "admin"}
+    return render_template("users/edit_user.html", users=removable_users)
+
+@app.route('/update_user', methods=['POST'])
+def update_user():
+    global users
+    username = request.form['user']
+    new_name = request.form['new_name']
+    new_password = request.form['new_password']
+
+    if username in users:
+        if new_name:
+            users.pop(username)
+            users[new_name] = None
+        elif new_password:
+            users.pop(username)
+            users[username] = new_password
+        elif new_name and new_password:
+            users.pop(username)
+            users[new_name] = new_password
     return render_template("users/users.html", users=users)
 
 # --------------- MAIN --------------- # 
